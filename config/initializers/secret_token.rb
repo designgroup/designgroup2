e7f1a02d16857d6d05ba786e29f93cb5be26fb88
Carolyn A. Edie <candi@designgroupink.com>
2011-11-29 19:24:57 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Designgroup2::Application.config.secret_token = 'ed4fdadaa6f0d0f078be4afbce7887d56be4c02ec8ab20eff207913e965fde1511fb5caec83a69c00825c9f233dedc1a5a73cc2159984ec9d80b2cb59832dfb2'
