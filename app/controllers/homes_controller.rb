2cc88c39f7b297d15b702fbe7fd355a410c6da85
Carolyn A. Edie <candi@designgroupink.com>
2011-11-30 19:33:04 -0800

    initial commit for designgroup2

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,7 @@
+class HomesController < ApplicationController
+  # GET /homes
+  # GET /homes.json
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Designgroup2::Application.routes.draw do
+  resources :homes
+  root :to => 'homes#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
